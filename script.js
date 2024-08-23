33c3f6a73dab9b841c04ba1915483a3cc6137425
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-08-23 16:59:25 -0300

    commit_sj

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
+async function criarConta() {
+    const email = document.querySelector('input[name="email"]').value;
+    const user = document.querySelector('input[name="usuario"]').value;
+    const passworld = document.querySelector('input[name="senha"]').value;
 
-    async function criarConta() {
-        // Capturando os dados do formulário
-        const email = document.querySelector('input[name="email"]').value;
-        const usuario = document.querySelector('input[name="usuario"]').value;
-        const senha = document.querySelector('input[name="senha"]').value;
+    const data = {
+        email: email,
+        user: user,
+        passworld: passworld
+    };
 
-        // Criando o objeto com os dados
-        const data = {
-            email: email,
-            usuario: usuario,
-            senha: senha
-        };
+    try {
+        const response = await fetch('http://localhost:3000/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
 
-        try {
-            // Enviando a requisição para a API
-            const response = await fetch('mongodb+srv://viniciusjosepereira:nPjlFE38EiPrVfgY@api-cadastro-usuario.unc0z.mongodb.net/?retryWrites=true&w=majority&appName=api-cadastro-usuario', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-
-            if (response.ok) {
-                alert('Conta criada com sucesso!');
-                // Redireciona para a página de login ou outra página
-                window.location.href = "index.html";
-            } else {
-                alert('Erro ao criar conta');
-            }
-        } catch (error) {
-            console.error('Erro:', error);
+        if (response.ok) {
+            alert('Conta criada com sucesso!');
+            window.location.href = "index.html";
+        } else {
             alert('Erro ao criar conta');
         }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao criar conta');
     }
+}
